97d6a67e06c4468f007b1ba586ee0eb76700c7a7
Laura Silva <72063248+LauraSilva1401@users.noreply.github.com>
2023-10-08 07:26:12 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
+
 }
 
 android {
@@ -40,6 +41,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
-    implementation("com.google.firebase:firebase-analytics")
+ 
 //    implementation("com.google.firebase:firebase-analytics")
 }
